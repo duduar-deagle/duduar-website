382163ebeff67cad29a6a1681b561b5309c8e37a
Duduar Deagle <122890455+duduar-deagle@users.noreply.github.com>
2023-02-24 22:11:17 -0600

    Project startup and basic style setup
    Website to be developed with a mobile-first approach.

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import "@styles/globals.css";
+import { Imprima, Lexend } from "next/font/google";
+import clsx from "clsx";
+
+const defaultFont = Imprima({
+  variable: "--font-default",
+  weight: "400",
+  subsets: ["latin-ext"],
+  display: "swap",
+});
+
+const headerFont = Lexend({
+  variable: "--font-header",
+  weight: "800",
+  subsets: ["latin-ext"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="es-419"
+      className={clsx(
+        defaultFont.variable,
+        headerFont.variable,
+        "scroll-smooth select-none"
+      )}
+    >
+      <body className="text-sm text-neutralWhite bg-carbon">
+        <div className="px-8">{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "375px",
+      ...defaultTheme.screens,
+    },
+    fontFamily: {
+      sans: ["var(--font-default)", ...defaultTheme.fontFamily.sans],
+      serif: ["var(--font-header)", ...defaultTheme.fontFamily.serif],
+    },
+    extend: {
+      colors: {
+        neutralWhite: "#e6e6e6",
+        neutralBlack: "#212121",
+        carbon: "#1c1c1c",
+      },
+    },
+  },
+};
